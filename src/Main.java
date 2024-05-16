7802a22107eeb9121330f7e7a7c566088a188720
wisoums <wissem_oumsalem@yahoo.ca>
2024-05-16 18:49:28 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,43 @@ public class Main {
             return (oddonacciMultiple(n-1) + oddonacciMultiple(n-2) + oddonacciMultiple(n-3));
         }
     }
+    public static int[] oddonacciLinear(int n)
+    {
+        int[] A = new int[3]; //initializing returned array
+        int i = 0, j = 0, k=0; //initializing variable i j k
+
+        if (n == 3 || n == 2 || n == 1) //1st 3 positions are 1 (base case)
+        {
+            i=1;
+            j=1;
+            k=1;
+
+            A[0] = i; A[1] = j; A[2]=k; //setting those values in the array
+            System.out.print(k + " " + j + " " + i + " ");
+
+            return (A);	//returning the updated array
+        }
+        else
+        {
+            A = oddonacciLinear(n - 1); //recursive call
+            i = A[0];
+            j = A[1];
+            k = A[2];
+
+            System.out.print((i+j+k) + " ");
+
+            A[0] = i + j + A[2];
+            A[1] = i;
+            A[2] = j;
+
+            return (A);
+        }
+    }
     public static void displayResultFile(){
         PrintWriter writer=null;
         try{
             writer = new PrintWriter(new FileOutputStream("OddoOut.txt"));
+
             writer.println("Result of the Oddonacci multiple recursion");
             writer.println();
 
@@ -25,7 +58,23 @@ public class Main {
 
             for (int i =5 ; i<=35 ; i+=5){
                 writer.println("************************");
-                writer.println("Oddonacci("+i+") :");
+                writer.println("OddonacciMultiple("+i+") :");
+                startTimeMultiple = System.nanoTime();
+                result=oddonacciMultiple(i);
+                endTimeMultiple = System.nanoTime();
+                writer.println("Result : " + result);
+                writer.println("Built-in time : " +(endTimeMultiple -startTimeMultiple) + " ms");
+                writer.println();
+            }
+            writer.println();
+            writer.println();
+
+            writer.println("Result of the Oddonacci linear recursion");
+            writer.println();
+
+            for (int i =5 ; i<=35 ; i+=5){
+                writer.println("************************");
+                writer.println("OddonacciLinear("+i+") :");
                 startTimeMultiple = System.nanoTime();
                 result=oddonacciMultiple(i);
                 endTimeMultiple = System.nanoTime();
